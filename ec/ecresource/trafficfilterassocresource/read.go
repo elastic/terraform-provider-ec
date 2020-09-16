52420fe40ed00708765cd9a9f83752df7a2dd661
Marc Lopez <marc5.12@outlook.com>
2020-09-16 14:12:54 +0300

    Add traffic_filter_association resource
    Adds a new `ec_deployment_traffic_filter_association` resource which can
    be used when an existing Elastic Cloud deployment lives outside of the
    realms of terraform. In that case, using this new resource might be
    desirable to manage its traffic rules and their association to said
    deployment.
    
    On the other hand, if this resource is used to reference a deployment
    which is already managed by terraform, it will cause a diff since a
    new traffic filter will be a added to `ec_deployment.traffic_filter`.
    
    Thus the note on usability on the resource document.
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

---
+++ ec/ecresource/trafficfilterassocresource/read.go
@@ -0,0 +1,47 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package trafficfilterassocresource
+
+import (
+	"context"
+
+	"github.com/elastic/cloud-sdk-go/pkg/api"
+	"github.com/elastic/cloud-sdk-go/pkg/api/deploymentapi/trafficfilterapi"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// Read queries the remote deployment traffic filter ruleset association and
+// updates the local state.
+func read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var client = meta.(*api.API)
+	res, err := trafficfilterapi.Get(trafficfilterapi.GetParams{
+		API:                 client,
+		ID:                  d.Get("traffic_filter_id").(string),
+		IncludeAssociations: true,
+	})
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := flatten(res, d); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
---
+++ ec/ecresource/trafficfilterassocresource/schema.go
@@ -0,0 +1,40 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package trafficfilterassocresource
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// NewSchema returns the schema for an "ec_deployment_traffic_filter_association" resource.
+func NewSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"deployment_id": {
+			Type:        schema.TypeString,
+			Description: `Required deployment identifier where the traffic filter will be associated`,
+			Required:    true,
+			ForceNew:    true,
+		},
+		"traffic_filter_id": {
+			Type:        schema.TypeString,
+			Description: "Required traffic filter ruleset identifier to tie to a deployment",
+			Required:    true,
+			ForceNew:    true,
+		},
+	}
+}
