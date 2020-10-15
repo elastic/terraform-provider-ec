a26695a937736d693ce18b5ee64f0f3bc118d3c9
ggsood <gauravsood@yahoo.com>
2020-10-15 03:02:21 +0100

    Fixing CCS cluster template

---
+++ ec/acc/testdata/deployment_basic_defaults_5.tf
@@ -0,0 +1,13 @@
+data "ec_stack" "latest" {
+  version_regex = "latest"
+  region        = "%s"
+}
+
+resource "ec_deployment" "defaults" {
+  name                   = "%s"
+  region                 = "%s"
+  version                = data.ec_stack.latest.version
+  deployment_template_id = "%s"
+
+  elasticsearch {}
+}
