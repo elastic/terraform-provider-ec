c0bd3c93c5d91b1c58a2bcb1e9f560a365cfed6d
Karen Cárcamo <karencfv@users.noreply.github.com>
2020-10-22 18:26:37 +1300

    Update ec/ecdatasource/deploymentdatasource/datasource.go
    Co-authored-by: Marc Lopez Rubio <marc5.12@outlook.com>

--- ec/ecdatasource/deploymentdatasource/datasource.go
+++ ec/ecdatasource/deploymentdatasource/datasource.go
@@ -104,7 +104,7 @@ func modelToState(d *schema.ResourceData, res *models.DeploymentGetResponse) err
 		}
 	}
 
-	if observability := flattenObservability(res.Settings); observability != nil {
+	if observability := flattenObservability(res.Settings); len(observability) > 0 {
 		if err := d.Set("observability", observability); err != nil {
 			return err
 		}
