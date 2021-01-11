bcb5a9153a2066d23174d6beea9a72665dcbde05
Marc Lopez Rubio <marc5.12@outlook.com>
2021-01-11 13:43:02 +0300

    Update ec/acc/deployment_destroy_test.go
    Co-authored-by: Karen Cárcamo <karencfv@users.noreply.github.com>

--- ec/acc/deployment_destroy_test.go
+++ ec/acc/deployment_destroy_test.go
@@ -78,7 +78,7 @@ func testAccDeploymentDestroy(s *terraform.State) error {
 						continue
 					}
 					merr = merr.Append(
-						fmt.Errorf("deployment [%s] resource enterpriseSearch (%s) still exists",
+						fmt.Errorf("deployment [%s] resource enterprise_search (%s) still exists",
 							rs.Primary.ID, *res.ID,
 						),
 					)
