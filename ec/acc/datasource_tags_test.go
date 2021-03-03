50f8f06ba2f82362576d3b708d3a5a335eb977f3
Neil Brookes <neil.brookes@elastic.co>
2021-03-03 11:30:18 +0000

    Acceptance test for tag filters

--- ec/acc/datasource_tags_test.go
+++ ec/acc/datasource_tags_test.go
@@ -31,12 +31,16 @@ import (
 // * Create a deployment resource with tags.
 // * Create a datasource from the resource.
 // * Ensure tags exist.
+// * Create a datasource with filters for the unique tag
+// * Ensure that only a single deployment is returned
 func TestAccDatasource_basic_tags(t *testing.T) {
 
-	datasourceName := "data.ec_deployment.tags"
+	datasourceName := "data.ec_deployment.tagdata"
+	filterDatasourceName := "data.ec_deployments.tagfilter"
 	depCfg := "testdata/datasource_tags.tf"
-	randomName := prefix + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
-	cfg := fixtureAccTagsDataSource(t, depCfg, randomName, getRegion(), defaultTemplate)
+	testID := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	randomName := prefix + testID
+	cfg := fixtureAccTagsDataSource(t, depCfg, randomName, getRegion(), defaultTemplate, testID)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
@@ -46,16 +50,28 @@ func TestAccDatasource_basic_tags(t *testing.T) {
 			{
 				Config: cfg,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(datasourceName, "tags.%", "2"),
+					// Check the "ec_deployment" datasource
+					resource.TestCheckResourceAttr(datasourceName, "tags.%", "3"),
 					resource.TestCheckResourceAttr(datasourceName, "tags.foo", "bar"),
 					resource.TestCheckResourceAttr(datasourceName, "tags.bar", "baz"),
+					resource.TestCheckResourceAttr(datasourceName, "tags.test_id", testID),
+				),
+			},
+			{
+				Config: cfg,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Check the "ec_deployments" datasource, which filters on tags.
+					resource.TestCheckResourceAttrSet(filterDatasourceName, "return_count"),
+					resource.TestCheckResourceAttr(filterDatasourceName, "return_count", "1"),
+					resource.TestCheckResourceAttr(filterDatasourceName, "deployments.#", "1"),
+					resource.TestCheckResourceAttrSet(filterDatasourceName, "deployments.0.deployment_id"),
 				),
 			},
 		},
 	})
 }
 
-func fixtureAccTagsDataSource(t *testing.T, fileName, name, region string, depTpl string) string {
+func fixtureAccTagsDataSource(t *testing.T, fileName, name, region string, depTpl string, testID string) string {
 	t.Helper()
 
 	deploymentTpl := setDefaultTemplate(region, depTpl)
@@ -64,6 +80,6 @@ func fixtureAccTagsDataSource(t *testing.T, fileName, name, region string, depTp
 		t.Fatal(err)
 	}
 	return fmt.Sprintf(string(b),
-		region, name, region, deploymentTpl,
+		region, name, region, deploymentTpl, testID, testID,
 	)
 }
