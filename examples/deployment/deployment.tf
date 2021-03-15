933b490f53df51069246ad503d55bd730bd2bbad
Omer Kushmaro <omer.kushmaro@elastic.co>
2021-03-15 11:19:50 +0200

    Added es_settings to examples (#254)
    Added a comment to the examples section clarifying why .12.29 is used
    Added an es_settings.yaml file to demonstrate usage of config in elasticsearch block.

--- examples/deployment/deployment.tf
+++ examples/deployment/deployment.tf
@@ -1,4 +1,6 @@
 terraform {
+  # The Elastic Cloud provider is supported from ">=0.12"
+  # Version later than 0.12.29 is required for this terraform block to work.
   required_version = ">= 0.12.29"
 
   required_providers {
@@ -21,19 +23,23 @@ resource "ec_deployment" "example_minimal" {
   version                = "7.9.2"
   deployment_template_id = "aws-io-optimized-v2"
 
-  elasticsearch {}
-
-  kibana {}
-
-  apm {
-    topology {
-      size = "0.5g"
+  elasticsearch {
+    config {
+      user_settings_yaml = file("./es_settings.yaml")
     }
   }
+
+  kibana {}
 
   enterprise_search {
     topology {
       zone_count = 1
+    }
+  }
+
+  apm {
+    topology {
+      size = "0.5g"
     }
   }
 }
