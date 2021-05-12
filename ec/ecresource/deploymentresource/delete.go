28607b842860e303bb6e9132b4039a7d4e953741
Marc Lopez Rubio <marc5.12@outlook.com>
2021-05-12 16:24:54 +0500

    ec_deployment: Retry destroy on Timeout Exceeded (#308)
    Adds retries when the plan tracking message of a deployment shutdown
    contains the Timeout Exceeded message, with a maximum of 3 retries.
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

--- ec/ecresource/deploymentresource/delete.go
+++ ec/ecresource/deploymentresource/delete.go
@@ -20,53 +20,78 @@ package deploymentresource
 import (
 	"context"
 	"errors"
+	"strings"
 
 	"github.com/elastic/cloud-sdk-go/pkg/api"
 	"github.com/elastic/cloud-sdk-go/pkg/api/deploymentapi"
 	"github.com/elastic/cloud-sdk-go/pkg/client/deployments"
 	"github.com/elastic/cloud-sdk-go/pkg/multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-// Delete shuts down and deletes the remote deployment.
-func deleteResource(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+// Delete shuts down and deletes the remote deployment retrying up to 3 times
+// the Shutdown API call in case the plan returns with a failure that contains
+// the "Timeout Exceeded" string, which is a fairly common transient error state
+// returned from the API.
+func deleteResource(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	const maxRetries = 3
+	var retries int
+	timeout := d.Timeout(schema.TimeoutDelete)
 	client := meta.(*api.API)
 
-	if _, err := deploymentapi.Shutdown(deploymentapi.ShutdownParams{
-		API: client, DeploymentID: d.Id(),
-	}); err != nil {
-		if alreadyDestroyed(err) {
-			d.SetId("")
-			return nil
+	return diag.FromErr(resource.RetryContext(ctx, timeout, func() *resource.RetryError {
+		if _, err := deploymentapi.Shutdown(deploymentapi.ShutdownParams{
+			API: client, DeploymentID: d.Id(),
+		}); err != nil {
+			if alreadyDestroyed(err) {
+				d.SetId("")
+				return nil
+			}
+			return resource.NonRetryableError(multierror.NewPrefixed(
+				"failed shutting down the deployment", err,
+			))
 		}
-		return diag.FromErr(multierror.NewPrefixed(
-			"failed shutting down the deployment", err,
-		))
-	}
 
-	if err := WaitForPlanCompletion(client, d.Id()); err != nil {
-		return diag.FromErr(err)
-	}
+		if err := WaitForPlanCompletion(client, d.Id()); err != nil {
+			if shouldRetryShutdown(err, retries, maxRetries) {
+				retries++
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
 
-	if err := handleTrafficFilterChange(d, client); err != nil {
-		return diag.FromErr(err)
-	}
+		if err := handleTrafficFilterChange(d, client); err != nil {
+			return resource.NonRetryableError(err)
+		}
 
-	// We don't particularly care if delete succeeds or not. It's better to
-	// remove it, but it might fail on ESS. For example, when user's aren't
-	// allowed to delete deployments, or on ECE when the cluster is "still
-	// being shutdown". Sumarizing, even if the call fails the deployment
-	// won't be there.
-	_, _ = deploymentapi.Delete(deploymentapi.DeleteParams{
-		API: client, DeploymentID: d.Id(),
-	})
+		// We don't particularly care if delete succeeds or not. It's better to
+		// remove it, but it might fail on ESS. For example, when user's aren't
+		// allowed to delete deployments, or on ECE when the cluster is "still
+		// being shutdown". Sumarizing, even if the call fails the deployment
+		// won't be there.
+		_, _ = deploymentapi.Delete(deploymentapi.DeleteParams{
+			API: client, DeploymentID: d.Id(),
+		})
 
-	d.SetId("")
-	return nil
+		d.SetId("")
+		return nil
+	}))
 }
 
 func alreadyDestroyed(err error) bool {
 	var destroyed *deployments.ShutdownDeploymentNotFound
 	return errors.As(err, &destroyed)
+}
+
+func shouldRetryShutdown(err error, retries, maxRetries int) bool {
+	const timeout = "Timeout exceeded"
+	needsRetry := retries < maxRetries
+
+	var isTimeout bool
+	if err != nil {
+		isTimeout = strings.Contains(err.Error(), timeout)
+	}
+	return isTimeout && needsRetry
 }
