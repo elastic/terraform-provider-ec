55dc12cc3dfc74dd231318e0281d2491d8f10693
Marc Lopez Rubio <marc5.12@outlook.com>
2021-09-10 10:34:38 +0200

    ec_deployment: Persist lowest resource `version` (#371)
    Reconciles the top level `ec_deployment.version` field to the lesser of
    all of the resource versions. Cases where the version is updated and one
    or more of the resources did not succeed to upgrade, were not detected
    since the `version` field was not reconciled during the Read stage and
    was set to the value the terraform configuration was set to without the
    ability to detect drift.
    
    Signed-off-by: Marc Lopez Rubio <marc5.12@outlook.com>
    Co-authored-by: David Kilfoyle <41695641+kilfoyle@users.noreply.github.com>

---
+++ ec/acc/testdata/deployment_upgrade_retry_1.tf
@@ -0,0 +1,26 @@
+locals {
+  region              = "%s"
+  deployment_template = "%s"
+}
+
+data "ec_stack" "latest" {
+  version_regex = "7.10.?"
+  region        = local.region
+}
+
+resource "ec_deployment" "upgrade_retry" {
+  name                   = "terraform_acc_upgrade_retry"
+  region                 = local.region
+  version                = data.ec_stack.latest.version
+  deployment_template_id = local.deployment_template
+
+  elasticsearch {
+    topology {
+      id         = "hot_content"
+      size       = "1g"
+      zone_count = 1
+    }
+  }
+
+  kibana {}
+}
