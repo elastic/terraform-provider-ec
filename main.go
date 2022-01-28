2cf6e60a6856213fad9e56f7db502187f8d66c69
Luigi Bitonti <luigi.bitonti@gmail.com>
2022-01-28 10:13:25 +0000

    feat: 429 add debug mode

--- main.go
+++ main.go
@@ -18,14 +18,29 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"github.com/elastic/terraform-provider-ec/ec"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"log"
 )
 
 //go:generate go run ./gen/gen.go
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: ec.Provider,
-	})
+	var debugMode bool
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{ProviderFunc: ec.Provider}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/elastic/ec", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
