0627d93d2f1e77e827447407db6a1f646ab5ed43
Dmytro Onishchenko <dmytro.onishchenko@elastic.co>
2022-09-06 14:33:52 +0200

    Introduce mux and port the provider to v6.

--- main.go
+++ main.go
@@ -18,10 +18,15 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/elastic/terraform-provider-ec/ec"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 )
 
 //go:generate go run ./gen/gen.go
@@ -30,13 +35,44 @@ import (
 const ProviderAddr = "registry.terraform.io/elastic/ec"
 
 func main() {
-	var debugMode bool
-	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	debugFlag := flag.Bool("debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: ec.Provider,
-		Debug:        debugMode,
-		ProviderAddr: ProviderAddr,
-	})
+	upgradedSdkProvider, err := tf5to6server.UpgradeServer(
+		context.Background(),
+		ec.Provider().GRPCProvider,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	providers := []func() tfprotov6.ProviderServer{
+		func() tfprotov6.ProviderServer { return upgradedSdkProvider },
+		// TODO
+		// add new v6 provider with `ec_deployment` resource
+	}
+
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, providers...)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serveOpts []tf6server.ServeOpt
+
+	if *debugFlag {
+		serveOpts = append(serveOpts, tf6server.WithManagedDebug())
+	}
+
+	err = tf6server.Serve(
+		ProviderAddr,
+		muxServer.ProviderServer,
+		serveOpts...,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
